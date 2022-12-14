418924492120485e925f2ef9528ee2390d3a46e8
piero965d <piero965d@gmail.com>
2022-12-14 03:24:45 -0500

    'style-tienda'

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -5,12 +5,12 @@ function Guitarra({guitarra}) {
 
   return (
     <div className="guitarra">
+        <img src={imagen.data.attributes.formats.medium.url} alt={`Imagen guitarra ${nombre}`} />
         <div className="contenido">
-            <img src={imagen.data.attributes.formats.medium.url} alt={`Imagen guitarra ${nombre}`} />
             <h3>{nombre}</h3>
             <p className="descripcion">{descripcion}</p>
             <p className="precio">${precio}</p>
-            <Link className="enlace" to={'/guitarras/${url}'}>Ver</Link>
+            <Link className="enlace" to={'/guitarras/${url}'}>Ver Producto</Link>
         </div>
     </div>
   )
--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,26 @@
 import { getGuitarras } from "~/models/guitrras.server"
 import { useLoaderData } from "@remix-run/react"
 import Guitarra from "~/components/guitarra"
+import styles from "../styles/guitarras.css" 
+
+export function meta(){
+  return(
+    {
+      title: 'GuitarLA - Tienda',
+      description: 'GuitarLA - Nuestra coleccion de guitarras'
+    }
+  )
+}
+
+export function links(){
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
 export async function loader(){
   const guitarras = await getGuitarras()
   return guitarras.data
